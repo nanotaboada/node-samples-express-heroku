63fa6df14e332131d920c8c7a68311b326b040c0
Nano Taboada <nanotaboada@users.noreply.github.com>
2021-06-24 05:57:35 -0300

    Added ESLint

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const fs = require('fs');
+
 const app = express();
-const host = ""
 const port = process.env.PORT || 9000;
+const books = fs.readFileSync('books.json');
+const songs = fs.readFileSync('songs.json');
 
-let books = fs.readFileSync('books.json');
-let songs = fs.readFileSync('songs.json');
-
-app.get('/books', function (request, response) {
-  response.status(200); 
+app.get('/books', (request, response) => {
+  response.status(200);
   response.send(JSON.parse(books));
-  });
+});
 
-  app.get('/songs', function (request, response) {
-    response.status(200); 
-    response.send(JSON.parse(songs));
-    });
+app.get('/songs', (request, response) => {
+  response.status(200);
+  response.send(JSON.parse(songs));
+});
 
-    app.listen(port, () => {
-        console.log(`[RUNNING] Node.js + Express 4 app listening at port ${port}`)
-    });
+app.listen(port);
